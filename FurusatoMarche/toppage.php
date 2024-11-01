ae2d35cdfaa4f84d11e06be69c5620c2b328da5b
Oki-2301123 <2301123@s.asojuku.ac.jp>
2024-11-01 12:43:21 +0900

    トップページとファンクションの作成

--- FurusatoMarche/toppage.php
+++ FurusatoMarche/toppage.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TopPage</title>
 </head>
+
 <body>
+    <?php
+    require_once 'function.php';
     
+    ?>
 </body>
+
 </html>
