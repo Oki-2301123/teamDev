f7ae56aa6929491371538ae652719da28f43bc73
Oki-2301123 <2301123@s.asojuku.ac.jp>
2024-11-06 10:47:33 +0900

    functionの作成20241106

--- FurusatoMarche/function.php
+++ FurusatoMarche/function.php
@@ -1,11 +1,30 @@
 <?php
 
 function head(){
+    echo '<p>';
     echo 
         '<a href=toppage.php>
             <img src="" alt="アイコンロゴ">
         </a>';//ふるマルのアイコン　クリックでトップページに遷移　by荒亀
 
     echo
-        '<form action="#"';
+        '<form action="toppage.php" method="get">
+            <input type="text" name="keyword" placeholder="キーワードから探す">
+            <input type="submit" name ="sarch" value="検索"
+        </form>';
+
+    echo '<a href=cart.php>
+            <button type="button"><img src="" alt="カートのアイコン"</button>
+        </a>';
+    echo '<a href=favorite.php>
+        <button type="button"><img src="" alt="カートのアイコン"</button>
+    </a>';
+    echo '<a href=mypage.php>
+            <button type="button"><img src="" alt="カートのアイコン"</button>
+        </a>';
+
+        echo '<a href=mypage.php>
+            <button type="button"><img src="" alt="カートのアイコン"</button>
+        </a>';
+    echo'</p>';
 }
