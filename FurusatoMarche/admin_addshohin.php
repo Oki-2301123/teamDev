09936692d6c9baf1e63c5576466148c68642f7a0
nomura-seiki <2301185@s.asojuku.ac.jp>
2024-11-08 12:44:39 +0900

    管理者追加

--- FurusatoMarche/admin_addshohin.php
+++ FurusatoMarche/admin_addshohin.php
@@ -6,6 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="admin_top.php" method="post">
+        商品名
+        <input type="text" name="name"><br>
+        単価
+        <input type="text" name="tanka">円<br>
+        在庫<input type="text" name="zaiko">個<br>
+        オプション<input type="text" name="op"><br>
+        商品説明<input type="text" name="setu"><br>
+        産地<input type="text" name="san"><br>
+        販売元<input type="text" name="han"><br>
+    </form>
 </body>
 </html>
