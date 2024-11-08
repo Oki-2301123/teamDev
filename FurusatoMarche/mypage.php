2366e0d9765050d1ad6ade189e638750434d6566
UTastuya <2301134@s.asojuku.ac.jp>
2024-11-08 11:49:23 +0900

    お支払い方法

--- FurusatoMarche/mypage.php
+++ FurusatoMarche/mypage.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>//打田
+<body>
     マイページ<br>
     <form action="tyumonrireki.php" method="post">
     <input type="submit" name="myrireki" value="注文履歴">
