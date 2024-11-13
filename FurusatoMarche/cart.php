ea5a175fb9fd13efdb254fefe3363739b20fa03c
yukako-ykk <2301188@s.asojuku.ac.jp>
2024-11-13 09:59:02 +0900

    ヘッダー呼び出し

--- FurusatoMarche/cart.php
+++ FurusatoMarche/cart.php
@@ -8,38 +8,41 @@
 </head>
 
 <body>//カート画面 林
-        <?php
-        echo '<table>';
-        //写真
-        echo $_GET[''];
-        //産地 カテゴリー
-        echo $_GET[''], '県産  ',$_GET[''],'<br>';
-        //単価
-        echo '<h3>', $_GET[''],'円' ,'</h3><br>';
-        echo '<h4>','送料無料','</h4>', '<br>';
-        //数量 ドロップダウン
-        echo '数量';
-        echo '<select name="num">';
-        for($i=1;$i<=100;$i++){
-            print ('<option value="'. $i.'</option>');
-        }
-        //削除ボタン
-        ?>
-        <button onclick="location.href='#'">削除</button>
-        <?php
-        echo '</table>';
+    <?php
+    require_once 'function.php';
+    head(); //ヘッダー呼び出し
 
-        //カートの中身の合計
-        echo '<h3>','合計',$_GET[''],'円';
-        //カートの中身一括削除ボタン
-        ?>
-        <button onclick="location.href='#'">一括削除</button>;
-        <?php
-        //レジに進むボタン
-        echo '<input type="submit" value="レジに進む">';
+    echo '<table>';
+    //写真
+    echo $_GET[''];
+    //産地 カテゴリー
+    echo $_GET[''], '県産  ', $_GET[''], '<br>';
+    //単価
+    echo '<h3>', $_GET[''], '円', '</h3><br>';
+    echo '<h4>', '送料無料', '</h4>', '<br>';
+    //数量 ドロップダウン
+    echo '数量';
+    echo '<select name="num">';
+    for ($i = 1; $i <= 100; $i++) {
+        print('<option value="' . $i . '</option>');
+    }
+    //削除ボタン
+    ?>
+    <button onclick="location.href='#'">削除</button>
+    <?php
+    echo '</table>';
 
-        ?>
-    
+    //カートの中身の合計
+    echo '<h3>', '合計', $_GET[''], '円';
+    //カートの中身一括削除ボタン
+    ?>
+    <button onclick="location.href='#'">一括削除</button>;
+    <?php
+    //レジに進むボタン
+    echo '<input type="submit" value="レジに進む">';
+
+    ?>
+
 </body>
 
 </html>
--- FurusatoMarche/create_account.php
+++ FurusatoMarche/create_account.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+    require_once 'function.php';
+    head();//ヘッダー呼び出し
+
     <h1>ふるマル</h1>
     <h2>無料会員登録</h2>
     <h3>会員情報入力→会員情報確認</h3>
@@ -27,62 +32,63 @@
         郵便番号<input type="text" name="yubin"><input type="submit" value="住所検索"><br><br>
         都道府県
         <select name="todouhu">
-    <option value="北海道">北海道</option>
-    <option value="青森県">青森県</option>
-    <option value="秋田県">秋田県</option>
-    <option value="岩手県">岩手県</option>
-    <option value="宮城県">宮城県</option>
-    <option value="山形県">山形県</option>
-    <option value="福島県">福島県</option>
-    <option value="茨城県">茨城県</option>
-    <option value="栃木県">栃木県</option>
-    <option value="群馬県">群馬県</option>
-    <option value="埼玉県">埼玉県</option>
-    <option value="千葉県">千葉県</option>
-    <option value="東京都">東京都</option>
-    <option value="神奈川県">神奈川県</option>
-    <option value="山梨県">山梨県</option>
-    <option value="長野県">長野県</option>
-    <option value="新潟県">新潟県</option>
-    <option value="富山県">富山県</option>
-    <option value="石川県">石川県</option>
-    <option value="福井県">福井県</option>
-    <option value="岐阜県">岐阜県</option>
-    <option value="静岡県">静岡県</option>
-    <option value="愛知県">愛知県</option>
-    <option value="三重県">三重県</option>
-    <option value="滋賀県">滋賀県</option>
-    <option value="京都府">京都府</option>
-    <option value="大阪府">大阪府</option>
-    <option value="兵庫県">兵庫県</option>
-    <option value="奈良県">奈良県</option>
-    <option value="和歌山県">和歌山県</option>
-    <option value="鳥取県">鳥取県</option>
-    <option value="島根県">島根県</option>
-    <option value="岡山県">岡山県</option>
-    <option value="広島県">広島県</option>
-    <option value="山口県">山口県</option>
-    <option value="徳島県">徳島県</option>
-    <option value="香川県">香川県</option>
-    <option value="愛媛県">愛媛県</option>
-    <option value="高知県">高知県</option>
-    <option value="福岡県">福岡県</option>
-    <option value="佐賀県">佐賀県</option>
-    <option value="長崎県">長崎県</option>
-    <option value="熊本県">熊本県</option>
-    <option value="大分県">大分県</option>
-    <option value="宮崎県">宮崎県</option>
-    <option value="鹿児島県">鹿児島県</option>
-    <option value="沖縄県">沖縄県</option>
-</select><br>
-市区町村<input type="text" name="siku"><br><br>
-番地<input type="text" name="banti"><br><br>
-マンション名<input type="text" name="man"><br><br>
-電話番号<input type=" text" name="ban"><br><br>
-<input type="submit" value="確認画面へ進む  >">
+            <option value="北海道">北海道</option>
+            <option value="青森県">青森県</option>
+            <option value="秋田県">秋田県</option>
+            <option value="岩手県">岩手県</option>
+            <option value="宮城県">宮城県</option>
+            <option value="山形県">山形県</option>
+            <option value="福島県">福島県</option>
+            <option value="茨城県">茨城県</option>
+            <option value="栃木県">栃木県</option>
+            <option value="群馬県">群馬県</option>
+            <option value="埼玉県">埼玉県</option>
+            <option value="千葉県">千葉県</option>
+            <option value="東京都">東京都</option>
+            <option value="神奈川県">神奈川県</option>
+            <option value="山梨県">山梨県</option>
+            <option value="長野県">長野県</option>
+            <option value="新潟県">新潟県</option>
+            <option value="富山県">富山県</option>
+            <option value="石川県">石川県</option>
+            <option value="福井県">福井県</option>
+            <option value="岐阜県">岐阜県</option>
+            <option value="静岡県">静岡県</option>
+            <option value="愛知県">愛知県</option>
+            <option value="三重県">三重県</option>
+            <option value="滋賀県">滋賀県</option>
+            <option value="京都府">京都府</option>
+            <option value="大阪府">大阪府</option>
+            <option value="兵庫県">兵庫県</option>
+            <option value="奈良県">奈良県</option>
+            <option value="和歌山県">和歌山県</option>
+            <option value="鳥取県">鳥取県</option>
+            <option value="島根県">島根県</option>
+            <option value="岡山県">岡山県</option>
+            <option value="広島県">広島県</option>
+            <option value="山口県">山口県</option>
+            <option value="徳島県">徳島県</option>
+            <option value="香川県">香川県</option>
+            <option value="愛媛県">愛媛県</option>
+            <option value="高知県">高知県</option>
+            <option value="福岡県">福岡県</option>
+            <option value="佐賀県">佐賀県</option>
+            <option value="長崎県">長崎県</option>
+            <option value="熊本県">熊本県</option>
+            <option value="大分県">大分県</option>
+            <option value="宮崎県">宮崎県</option>
+            <option value="鹿児島県">鹿児島県</option>
+            <option value="沖縄県">沖縄県</option>
+        </select><br>
+        市区町村<input type="text" name="siku"><br><br>
+        番地<input type="text" name="banti"><br><br>
+        マンション名<input type="text" name="man"><br><br>
+        電話番号<input type=" text" name="ban"><br><br>
+        <input type="submit" value="確認画面へ進む  >">
 
 
-        </form>
-        //newaccountを作った by野村
+    </form>
+    //newaccountを作った by野村
 </body>
+
 </html>
