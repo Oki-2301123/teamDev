589f27a64b116373d2b33dbb9ce138523f5ee777
Oki-2301123 <2301123@s.asojuku.ac.jp>
2024-11-13 12:38:38 +0900

    css

--- FurusatoMarche/logout.php
+++ FurusatoMarche/logout.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/stayle.css">
+    <link rel="stylesheet" href="../css/stayle.css"><!--css読み込み-->
 </head>
 
 <body><?php require_once 'function.php';
