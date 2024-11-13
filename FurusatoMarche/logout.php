f529281bf94677dde838c0665c170b3ce87d42e0
yukako-ykk <2301188@s.asojuku.ac.jp>
2024-11-13 12:25:34 +0900

    変更

--- FurusatoMarche/logout.php
+++ FurusatoMarche/logout.php
@@ -4,21 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Document</title><!-- 外部CSSファイルをリンク -->
+    <link rel="stylesheet" href="../css/styl.css">
 </head>
 
-<body>
-    require_once 'function.php';
-    head();//ヘッダー呼び出し
-
-    <img src="" alt=""><br>
-    <h2>ログアウト画面</h2><br>
-    <img src="" alt=""><br>
-    <form action="login.php" method="get">
-        <input type="submit" name="logout" value="ログアウト">
-    </form>//by打田
-
-
+<body><?php require_once 'function.php';
+        head(); // ヘッダー呼び出し     
+        ?><b>サンプルテキスト</b><img src="" alt=""><br>
+    <h2>ログアウト画面</h2><br><img src="" alt=""><br>
+    <form action="login.php" method="get"><input type="submit" name="logout" value="ログアウト"></form> <!-- by打田 -->
 </body>
 
 </html>
