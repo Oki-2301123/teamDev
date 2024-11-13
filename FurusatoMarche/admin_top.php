fea50ad9027fa852ef915d143439dc2c9e95ee4f
Oki-2301123 <2301123@s.asojuku.ac.jp>
2024-11-13 11:58:11 +0900

    野村の尻ぬぐい

--- FurusatoMarche/admin_top.php
+++ FurusatoMarche/admin_top.php
@@ -1,86 +1,78 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="admin_addshohin.php" method="post">
-        <!-- css -->
-        <link rel="stylesheet" href="./styles.css">
-    </head>
-    <body>
-        <header>
-            <div class="container">
-                <div class="header-logo">
-                    <h1><a href="toppage.php">ふるマル</a></h1>
-                </div>
 
-                <form action="logout.php" method="post">
-                    <input type="subit" value="ログアウト"></a>
-                </form>
-        </header>
-        <main>
-            <div class="wrapper">
-                <div class="container">
-                    <div class="wrapper-title">
-                         <h3>管理者画面</h3>
-                    </div>
-                    <div class="list">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>id</th>
-                                      <th>商品ID</th>
-                                      <th>商品名</th>
-                                      <th>在庫</th>
-                                      <th>単価</th>
-                                      <th>産地</th>
-                                    <th>販売元</th>
-                                    <th>オプション</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                   <?php foreach($news as $new): ?>
-                                   <?php foreach($products as $product): ?>
-                                <tr>
-                                      <td><?php echo $new['id']; ?></td>
-                                      <td><?php echo $new['shouhin_id']; ?></td>
-                                      <td><?php echo $new['shouhin_mei']; ?></td>
-                                      <td><?php echo $new[' stock']; ?></td>
-                                      <td><?php echo $product['price']; ?></td>
-                                      <td><?php echo $product['Productionarea']; ?></td>
-                                      <td><?php echo $product['Seller']; ?></td>
-                                      <td><?php echo $product['option']; ?></td>
-                                   <td>
-                                           <button class="btn btn-green" onclick="location.href='edit_news.php?id=<?php echo $new['id']; ?>'">編集</button>
-                                           </form>
-                                           <button class="btn btn-green">編集</button>
-                                    </td>
-                                </tr>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- css -->
+    <link rel="stylesheet" href="./styles.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="header-logo">
+                <h1><a href="toppage.php">ふるマル</a></h1>
+            </div>
+            <form action="logout.php" method="post">
+                <input type="submit" value="ログアウト">
+            </form>
+        </div>
+    </header>
+
+    <main>
+        <div class="wrapper">
+            <div class="container">
+                <div class="wrapper-title">
+                    <h3>管理者画面</h3>
+                </div>
+                <div class="list">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>商品ID</th>
+                                <th>商品名</th>
+                                <th>在庫</th>
+                                <th>単価</th>
+                                <th>産地</th>
+                                <th>販売元</th>
+                                <th>オプション</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($news as $new): ?>
+                                <?php foreach ($products as $product): ?>
+                                    <tr>
+                                        <td><?php echo $new['id']; ?></td>
+                                        <td><?php echo $new['shouhin_id']; ?></td>
+                                        <td><?php echo $new['shouhin_mei']; ?></td>
+                                        <td><?php echo $new['stock']; ?></td>
+                                        <td><?php echo $product['price']; ?></td>
+                                        <td><?php echo $product['Productionarea']; ?></td>
+                                        <td><?php echo $product['Seller']; ?></td>
+                                        <td><?php echo $product['option']; ?></td>
+                                        <td>
+                                            <button class="btn btn-green" onclick="location.href='edit_news.php?id=<?php echo $new['id']; ?>'">編集</button>
+                                        </td>
+                                    </tr>
                                 <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        </main>
-        <footer>
-            <div class="container">
-                <p>Copyright @ 2018 SQUARE, inc</p>
-            </div>
-        </footer>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container">
+            <p>Copyright @ 2018 SQUARE, inc</p>
+        </div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
-</html>
-</body>
+
 </html>
