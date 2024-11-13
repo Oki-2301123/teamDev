80f9ec13f59b43f09df21ce14b14499ee5ab25db
Oki-2301123 <2301123@s.asojuku.ac.jp>
2024-11-13 12:36:18 +0900

    css変更

--- FurusatoMarche/logout.php
+++ FurusatoMarche/logout.php
@@ -4,36 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Document</title><!-- 外部CSSファイルをリンク -->
-    <link rel="stylesheet" href="../css/styl.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/stayle.css">
 </head>
 
 <body><?php require_once 'function.php';
         head(); // ヘッダー呼び出し     
-        ?><b>サンプルテキスト</b><img src="" alt=""><br>
+        ?>
+    サンプルテキスト
+    <img src="" alt=""><br>
     <h2>ログアウト画面</h2><br><img src="" alt=""><br>
     <form action="login.php" method="get"><input type="submit" name="logout" value="ログアウト"></form> <!-- by打田 -->
-=======
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/stayl.css">
-</head>
-
-<body>
-    <?php
-    require_once 'function.php';
-    head();//ヘッダー呼び出し
-    ?>
-
-    <img src="" alt=""><br>
-    <h2>ログアウト画面</h2><br>
-    <img src="" alt=""><br>
-    <form action="login.php" method="get">
-        <input type="submit" name="logout" value="ログアウト">
-    </form><!--by打田-->
-
-
->>>>>>> 063da16fcd8b289b37753fa56c83fadb6a301d54
 </body>
 
 </html>
