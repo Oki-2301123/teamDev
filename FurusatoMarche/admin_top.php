ffef16689b489795888541897c0be70d332bf6ce
nomura-seiki <2301185@s.asojuku.ac.jp>
2024-11-13 11:59:09 +0900

    a

--- FurusatoMarche/admin_top.php
+++ FurusatoMarche/admin_top.php
@@ -1,20 +1,26 @@
 <!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="admin_addshohin.php" method="post">
+    <html>
+        <head>
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-13xxxxxxxxx"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-13xxxxxxxxx');
+            </script>
+
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+-          <title>記事管理</title>
++          <title>商品管理</title>
+
+        <link rel="icon" href="favicon.ico">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
         <!-- css -->
         <link rel="stylesheet" href="./styles.css">
     </head>
@@ -22,49 +28,63 @@
         <header>
             <div class="container">
                 <div class="header-logo">
-                    <h1><a href="toppage.php">ふるマル</a></h1>
+                    <h1><a href="dashboard.php">管理画面</a></h1>
                 </div>
 
-                <form action="logout.php" method="post">
-                    <input type="subit" value="ログアウト"></a>
-                </form>
+                <nav class="menu-right menu">
+                    <a href="logout.php">ログアウト</a>
+                </nav>
+            </div>
         </header>
         <main>
             <div class="wrapper">
                 <div class="container">
                     <div class="wrapper-title">
-                         <h3>管理者画面</h3>
+-                          <h3>記事管理</h3>
++                          <h3>商品管理</h3>
                     </div>
+-                       <button class="btn btn-blue" onclick="location.href='create_news.php'">投稿する</button>
++                       <button class="btn btn-blue" onclick="location.href='create_product.php'">商品登録する</button>
                     <div class="list">
                         <table>
                             <thead>
                                 <tr>
                                     <th>id</th>
-                                      <th>商品ID</th>
-                                      <th>商品名</th>
-                                      <th>在庫</th>
-                                      <th>単価</th>
-                                      <th>産地</th>
-                                    <th>販売元</th>
-                                    <th>オプション</th>
+-                                      <th>タイトル</th>
+-                                      <th>本文</th>
++                                      <th>商品名</th>
++                                      <th>説明文</th>
++                                      <th>単価</th>
++                                      <th>画像パス</th>
+                                    <th>更新日時</th>
+                                    <th>作成日時</th>
+                                    <th>操作</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                   <?php foreach($news as $new): ?>
-                                   <?php foreach($products as $product): ?>
+-                                   <?php foreach($news as $new): ?>
++                                   <?php foreach($products as $product): ?>
                                 <tr>
-                                      <td><?php echo $new['id']; ?></td>
-                                      <td><?php echo $new['shouhin_id']; ?></td>
-                                      <td><?php echo $new['shouhin_mei']; ?></td>
-                                      <td><?php echo $new[' stock']; ?></td>
-                                      <td><?php echo $product['price']; ?></td>
-                                      <td><?php echo $product['Productionarea']; ?></td>
-                                      <td><?php echo $product['Seller']; ?></td>
-                                      <td><?php echo $product['option']; ?></td>
+-                                      <td><?php echo $new['id']; ?></td>
+-                                      <td><?php echo $new['title']; ?></td>
+-                                      <td><?php echo $new['content']; ?></td>
+-                                      <td><?php echo $new['created_at']; ?></td>
+-                                      <td><?php echo $new['updated_at']; ?></td>
++                                      <td><?php echo $product['id']; ?></td>
++                                      <td><?php echo $product['product_name']; ?></td>
++                                      <td><?php echo $product['text']; ?></td>
++                                      <td><?php echo $product['price']; ?></td>
++                                      <td><?php echo $product['img_path']; ?></td>
++                                      <td><?php echo $product['updated_at']; ?></td>
++                                      <td><?php echo $product['created_at']; ?></td>
                                    <td>
-                                           <button class="btn btn-green" onclick="location.href='edit_news.php?id=<?php echo $new['id']; ?>'">編集</button>
-                                           </form>
-                                           <button class="btn btn-green">編集</button>
+-                                           <button class="btn btn-green" onclick="location.href='edit_news.php?id=<?php echo $new['id']; ?>'">編集</button>
+-                                           <button class="btn btn-red delete" data-id=<?php echo $new['id']; ?>>削除</button>
+-                                           <form method="POST" action="./delete_news.php" id="delete_form_<?php echo $new['id']; ?>">
+-                                               <input type="hidden" value="<?php echo $new['id']; ?>" name="id">
+-                                           </form>
++                                           <button class="btn btn-green">編集</button>
++                                           <button class="btn btn-red" >削除</button>
                                     </td>
                                 </tr>
                                 <?php endforeach; ?>
@@ -80,7 +100,5 @@
             </div>
         </footer>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</body>
-</html>
-</body>
+    </body>
 </html>
