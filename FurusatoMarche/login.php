60c135a20cbd1505b2ceb1aecaf945139fee453a
yukako-ykk <2301188@s.asojuku.ac.jp>
2024-11-18 12:27:23 +0900

    loginのcss作成

--- FurusatoMarche/login.php
+++ FurusatoMarche/login.php
@@ -4,31 +4,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/stayle.css"><!--css接続 -->
+    <link rel="stylesheet" href="../css/stayle.css">
     <title>Document</title>
 </head>
-
+<!-- フロント 林 -->
 <body>
-    <div class="container">
-    <h1>ふるマル</h1>
-    <div class="box">
-    <div style="border:1px solid #c9c8c8; padding:18px; margin: 50px; width: 50%;">
-    <h2>会員ログイン</h2>
+   
+<div style="text-align: center; margin-top: 20px;"><img src="../img/hurumaru_title.png" width="10%" height="5%" alt="アイコンロゴ"></div>
+<hr size="50" color="red">
+   
+<div class="box1">
+   <p> <h2>会員ログイン</h2>
     メールアドレス<input type="text" name="mailaddres"><br>
     パスワード<input type="text" name="pass"><br>
-    <form action="toppage.php" method="post">
+    <form action="toppage.php" method="post" class="button-form">
         <input type="submit" value="ログイン" >
     </form>
     <form action="admin_top.php" method="post">
         <input type="submit" value="管理者としてログイン"><br>
     </form>
-    テキストを入力</div>
+    </p>
+</div>
+
+<div class="">
+    テキストを入力
     ふるさとマルシェの会員登録されていない方
     <form action="create_account.php" method="post">
         <input type="submit" value="新規登録（無料）">
     </form>
-    </div>
-    </div>
+</div>  
+    
 </body>
 
 </html>
