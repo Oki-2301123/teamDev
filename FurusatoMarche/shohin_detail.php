24af55bf3e93dc26774a2d4be9a87d76045928f4
Oki-2301123 <2301123@s.asojuku.ac.jp>
2024-11-21 02:40:38 +0900

    a

--- FurusatoMarche/shohin_detail.php
+++ FurusatoMarche/shohin_detail.php
@@ -4,9 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/stayle.css">
-    <link rel="stylesheet" href="../css/header.css">
     <title></title><!-- 野村 -->
 </head>
 
@@ -26,40 +23,36 @@
     </div>
 
     <?php
-    $id=$_GET['id'];
+    $id = $_GET['id'];
     $pdo = pdo();
     $sql = 'SELECT * FROM shohins WHERE shohin_id=?';
-    $data=$pdo->prepare($sql);
-    $data->execute($id);
+    $data = $pdo->prepare($sql);
+    $data->execute([$id]);
 
-    foreach($data as $info){
+    foreach ($data as $info) {
         $imagePath = '/teamDev/uploads/' . $info['shohin_pict'];
-        echo '<img src="' . $imagePath . '" alt="' . $info['shohin_name'] . '>';
-        echo '<h2>'.$shohin_name.'</h2><br>';
-        echo '<h2 class="shohin_price">'.$info['shohin_price'].'円';//文字の色を赤　.shohin_priceで呼び出す
+        echo '<img src="' . $imagePath . '" alt="' . $info['shohin_name'] . '　class="shohin-image" width="50%" height="auto">';
+        echo '<h2>' . $info['shohin_name'] . '</h2><br>';
+        echo '<h2 class="shohin_price">' . $info['shohin_price'] . '円'; //文字の色を赤　.shohin_priceで呼び出す
         echo '<h3>商品説明</h3>';
-        echo '<shohin_detail_box>'.$info[''].'';
+        echo '<div class="shohin_detail_box">' . $info['shohin_explain'] . '</div>'; //boxをに入れる
+    
+    echo '<form action="order.php" method="post">';
+
+
+        echo '数量<select name="quant">';
+            for($i=1;$i>=$info['stock'];$i++){
+                echo '<option value="'.$i.'">'.$i.'</option>';
+            }
+        echo '</select>';
+        echo '<input type="submit" value="レジへ進む">';
+        echo '<form action="cart.php" method="post">';
+            echo '<input type="submit" value="戻る">';
+        echo '</form>';
+    echo '</form>';
+    echo '<a href="toppage.php"><button type="button">戻る</button></a>';
     }
     ?>
-    <form action="order.php" method="post">
-
-
-        数量<select name="suu">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-        </select>
-        <input type="submit" value="レジへ進む">
-        <form action="cart.php" method="post">
-            <input type="submit" value="戻る">
-        </form>
-    </form>
 </body>
 
 </html>
