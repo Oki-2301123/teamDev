f47518e8dba0ad87ab6244cdf1ce7919b3ef9a1b
UTastuya <2301134@s.asojuku.ac.jp>
2024-12-04 11:16:56 +0900

    a

--- FurusatoMarche/admin_addshohin.php
+++ FurusatoMarche/admin_addshohin.php
@@ -15,33 +15,37 @@
 <img src="../img/hurumaru_title.png" alt="アイコンロゴ">
 </div>
 <hr class="hr">
-    <form action="admin_top.php" method="post">
-    
+<form action="admin_insert.php" method="post" enctype="multipart/form-data">
     <div class="text">
         <label class="item-label2">商品追加</label>
     </div>
-<div class="parent">
-    <div class="box2">
-        <label class="item-label">商品名　</label>
-        <input type="text" name="shohin" style="width: 625px; height: 48px;"><br><br>
-        <label class="item-label">単価　</label>
-        <input type="text" name="tanka" style="width: 230px; height: 48px;">
-        <span class="item-label2">円</span><br>
+    <div class="parent">
+        <div class="box2">
+            <label class="item-label">商品名　</label>
+            <input type="text" name="name" style="width: 625px; height: 48px;"><br><br>
+            <label class="item-label">単価　</label>
+            <input type="text" name="price" style="width: 230px; height: 48px;">
+            <span class="item-label2">円</span><br>
+        </div>
     </div>
-</div>
-        <label class="item-label2">在庫　</label>
-        <input type="text" name="zaiko" style="width: 230px; height: 48px;">
-        <span class="item-label2">円</span><br>
-        <label class="item-label">オプション　</label>
-        <input type="text" name="opsyon" style="width: 230px; height: 48px;"><br>
-        <label class="item-label2">商品説明　</label>
-        <input type="text" name="setumei" style="width: 541px; height: 114px;"><br>
-        <label class="item-label">産地　</label>
-        <input type="text" name="santi" style="width: 230px; height: 48px;"><br>
-        <label class="item-label">販売元　</label>
-        <input type="text" name="hanbai" style="width: 230px; height: 48px;"><br>
-        <input type="submit" name="toroku" value="登録" class="buttun-kanri">
-        <input type="submit" name="modoru" value="戻る" class="buttun-kanri2">
-     </form><!--by打田 -->
+    <label class="item-label2">在庫　</label>
+    <input type="text" name="stock" style="width: 230px; height: 48px;">
+    <span class="item-label2"></span><br>
+    <label class="item-label">オプション　</label>
+    <input type="text" name="option" style="width: 230px; height: 48px;"><br>
+    <label class="item-label2">商品説明　</label>
+    <input type="text" name="explain" style="width: 541px; height: 114px;"><br>
+    <label class="item-label">産地　</label>
+    <input type="text" name="made" style="width: 230px; height: 48px;"><br>
+    <label class="item-label">販売元　</label>
+    <input type="text" name="seller" style="width: 230px; height: 48px;"><br>
+    <label class="item-label">写真　</label>
+    <input type="file" name="pict"><br>
+    <input type="submit" name="toroku" value="登録" class="buttun-kanri">
+</form>
+<form action="admin_top.php" method="post">
+    <input type="submit" name="modoru" value="戻る" class="buttun-kanri2">        
+</form>
+     <!--by打田 -->
 </body>
 </html>
