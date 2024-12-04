1e009d4b25e30b9941f3b1cd24999e6e50f41164
nomura-seiki <2301185@s.asojuku.ac.jp>
2024-12-04 09:57:08 +0900

    delete作成

--- FurusatoMarche/delete_shohin_check.php
+++ FurusatoMarche/delete_shohin_check.php
@@ -7,8 +7,14 @@
     <title>Document</title>
 </head>
 <body>
+<?php 
 require_once 'function.php';
+$pdo = pdo();
+$shohin_id = $_POST['shohin_id'];
+$sql = $pdo->prepare('DELETE FROM shohins WHERE shohin_id = ?');
+$sql->execute([$shohin_id]);
 head();//ヘッダー呼び出し
+?>
     
 </body>
 </html>
