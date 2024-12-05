7ebcb6f136a08553db51085885dc5f6ae2c97f61
yukako-ykk <2301188@s.asojuku.ac.jp>
2024-12-05 15:56:21 +0900

    ヘッダー作成

--- FurusatoMarche/toppage.php
+++ FurusatoMarche/toppage.php
@@ -65,6 +65,7 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/toppage.css">
     <script src="../js/toppage.js" defer></script>
