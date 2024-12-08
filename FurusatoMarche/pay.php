2be0a4a538f095adac17878ef15811b41ceb7758
Oki-2301123 <2301123@s.asojuku.ac.jp>
2024-12-09 04:26:54 +0900

    a

--- FurusatoMarche/pay.php
+++ FurusatoMarche/pay.php
@@ -6,70 +6,83 @@ require_once 'function.php'; // 外部ファイルから関数を読み込む
 
 // データベース接続を取得
 $pdo = pdo();
-?>
-<!DOCTYPE html>
-<html lang="en">
+if (isset($_SESSION['user_id'])) {
+    $sql = "SELECT * FROM carts WHERE users_id = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_SESSION['user_id']]);
+    $cart_check = $stmt->fetch();
+    if (!($cart_check)) {
+        $SESSION['msg'] = "カートが見つかりません";
+        header('Location: toppage.php');
+        exit;
+    }
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    $sql = "SELECT * FROM users WHERE user_id = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_SESSION['user_id']]);
+    $user_check = $stmt->fetch();
+    if (!($user_check)) {
+        $SESSION['msg'] = "ユーザーが見つかりません";
+        header('Location: toppage.php');
+        exit;
+    }
+    $users_id = $_SESSION['user_id'];
+    $carts_id = $cart_check['cart_id'];
+    $order_date = date('y-m-d');
+    $order_pay = 'クレジット';
+    $order_sent = $user_check['user_pref'] . $user_check['user_city'] . $user_check['user_address'] . $user_check['user_building'];
+    $rand = rand(1, 7);
+    $order_delive = date($order_date, strtotime("YYYY-mm-dd " . $rand . " day"));
+    $order_fee = $cart_check['cart_total'];
 
-<body>
-    <?php
-    head(); // ヘッダー呼び出し
-    ?>
-    <div class="subject-line">
-        <div class="subject">
-            <item class="subject1">ランキング</item>
-            <item class="subject2">地域で探す</item>
-            <item class="subject3">カテゴリ別</item>
-            <item class="subject4">セール商品</item>
-            <item class="subject5">特集</item>
-        </div>
-    </div>
-
-    <?php
-    if (isset($_SESSION['user_id'])) {
-        $sql = "SELECT * FROM carts WHERE users_id = ?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$_SESSION['user_id']]);
-        $cart_check = $stmt->fetch();
-        if ($cart_check) {
-            $SESSION['msg'] = "カートが見つかりません";
-            echo 'cart';
-        }
-        $sql = "SELECT * FROM users WHERE users_id = ?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$_SESSION['user_id']]);
-        $user_check = $stmt->fetch();
-        if ($user_check) {
-            $SESSION['msg'] = "ユーザーが見つかりません";
-            echo 'user';
-        }
-        $users_id = $_SESSION['user_id'];
-        $carts_id = $cart_check['cart_id'];
-        $order_date = date('y-m-d');
-        $order_pay = 'クレジット';
-        $order_sent = $user_check['user_pref'] . $user_check['user_city'] . $user_check['user_address'] . $user_check['user_building'];
-        $rand = rand(1, 7);
-        $order_delive = date($order_date, strtotime("YYYY-mm-dd " . $rand . " day"));
-        $order_fee = $cart_check['cart_total'];
-
-        $sql = 'INSERT INTO orders
-            (users_id,carts_id,order_date,order_pay,order_sent,order_deliver,order_fee) 
+    $sql = 'INSERT INTO orders
+            (users_id,carts_id,order_date,order_pay,order_sent,order_delive,order_fee) 
             VALUE(?,?,?,?,?,?,?)';
-        $stmt = $pdo->prepare($sql);
-        if (!($stmt->execute([$users_id, $carts_id, $order_date, $order_pay, $order_sent, $order_delive, $order_fee]))) {
-            $SESSION['msg'] = "注文の作成に失敗しました";
-            echo 'order';
+    $stmt = $pdo->prepare($sql);
+    if (!($stmt->execute([$users_id, $carts_id, $order_date, $order_pay, $order_sent, $order_delive, $order_fee]))) {
+        $SESSION['msg'] = "注文の作成に失敗しました";
+        header('Location: toppage.php');
+        exit;
+    }
+    $sql = 'SELECT * FROM orders WHERE carts_id = ?';
+    $orders_stmt = $pdo->prepare($sql);
+    $orders_stmt->execute([$carts_id]);
+    foreach ($orders_stmt as $data) {
+        $orders_id = $data['order_id'];
+    }
+
+    $sql = 'SELECT * FROM cart_details WHERE carts_id = ?';
+    $cart_stmt = $pdo->prepare($sql);
+    if (!($cart_stmt->execute([$carts_id]))) {
+        $SESSION['msg'] = "カートの取り出しに失敗しました";
+        header('Location: toppage.php');
+        exit;
+    }
+
+    foreach ($cart_stmt as $data) {
+        $carts_id = $data['carts_id'];
+        $shohins_id = $data['shohins_id'];
+        $order_quant = $data['cart_de_quant'];
+        $shohins_price = $data['shohins_price'];
+
+        $sql = 'INSERT INTO order_details
+            (orders_id,shohins_id,order_quant,shohins_price)
+            VALUE(?,?,?,?)';
+        $create_orders_de = $pdo->prepare($sql);
+        if (!($create_orders_de->execute([$orders_id, $shohins_id, $order_quant, $shohins_price]))) {
+            $SESSION['msg'] = "注文詳細の作成に失敗しました";
+            header('Location: toppage.php');
             exit;
         }
-        //order_de_idを作成（cart_detailからひとつずつ取り出して挿入）
-        //すべて入れたらcartテーブルを消す（WHERE＝user_id)
+        $sql = 'DELETE FROM cart_details WHERE shohins_id=? and carts_id = ?';
+        $delete_cart_de = $pdo->prepare($sql);
+        if (!($delete_cart_de->execute([$shohins_id, $carts_id]))) {
+            $SESSION['msg'] = "カート詳細の削除に失敗しました";
+            header('Location: toppage.php');
+            exit;
+        }
     }
-    ?>
-</body>
-
-</html>
+    $SESSION['msg'] = "購入完了しました。";
+    header('Location: toppage.php');
+    exit;
+}
