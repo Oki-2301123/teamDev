e91d6f45c77f07b850ac1e927a74505cf5ba9281
Oki-2301123 <2301123@s.asojuku.ac.jp>
2024-12-12 05:16:06 +0900

    a

--- FurusatoMarche/admin_shohin.php
+++ FurusatoMarche/admin_shohin.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/stayle.css"><!--css接続 -->
     <link rel="stylesheet" href="../css/reset.css"><!--css接続 -->
     <link rel="stylesheet" href="../css/style.css"><!--css接続 -->
     <link rel="stylesheet" href="../css/admin_shohin.css"><!--css接続 -->
@@ -19,50 +18,77 @@
     <hr class="hr">
     <form action="admin_shohins_insert.php" method="post" enctype="multipart/form-data">
         <div class="text">
-            <div class="text">
-                <label class="item-label2">商品情報</label>
-            </div>
-            <?php
-            require_once 'function.php';
-            if (isset($_POST['shohin_id'])) {
-                $shohin_id = $_POST['shohin_id'];
-                $pdo = pdo();
-                $sql = $pdo->prepare('SELECT * FROM shohins WHERE shohin_id = ?');
-                $sql->execute([$shohin_id]);
-                $product = $sql->fetch(PDO::FETCH_ASSOC);
+            <label class="item-label2">商品情報</label>
+        </div>
+        <?php
+        require_once 'function.php';
+        if (isset($_POST['shohin_id'])) {
+            $shohin_id = $_POST['shohin_id'];
+            $pdo = pdo();
+            $sql = $pdo->prepare('SELECT * FROM shohins WHERE shohin_id = ?');
+            $sql->execute([$shohin_id]);
+            $product = $sql->fetch(PDO::FETCH_ASSOC);
 
-                // 取得した商品情報をフォームに表示
-                if ($product) {
-                    $id = $product['shohin_id'];
-                    $name = $product['shohin_name'];
-                    $price = $product['shohin_price'];
-                    $stock = $product['shohin_stock'];
-                    $option = $product['shohin_option'];
-                    $category = $product['shohin_category'];
-                    $shohin_pict = $product['shohin_pict'];
-                    $explain = $product['shohin_explain'];
-                    $made = $product['shohin_made'];
-                    $seller = $product['shohin_seller'];
-                }
+            // 取得した商品情報をフォームに表示
+            if ($product) {
+                $id = $product['shohin_id'];
+                $name = $product['shohin_name'];
+                $price = $product['shohin_price'];
+                $stock = $product['shohin_stock'];
+                $option = $product['shohin_option'];
+                $category = $product['shohin_category'];
+                $shohin_pict = $product['shohin_pict'];
+                $explain = $product['shohin_explain'];
+                $made = $product['shohin_made'];
+                $seller = $product['shohin_seller'];
             }
-            ?>
-            商品名<input type="text" name="name" value="<?= $name ?>" style="width: 450px; height: 48px;"><br><br>
-            単価<input type="text" name="price" value="<?= $price ?>" style="width: 230px; height: 48px;">円<br><br>
-            在庫<input type="text" name="stock" value="<?= $stock ?>" style="width: 230px; height: 48px;">個<br><br>
-            オプション<input type="text" name="option" value="<?= $option ?>" style="width: 230px; height: 48px;"><br><br>
-            カテゴリー<input type="text" name="category" value="<?= $category ?>" style="width: 230px; height: 48px;"><br><br>
-            商品画像<?php
+        }
+        ?>
+        <div class="form-container">
+            <div class="form-left">
+                <input type="hidden" name="id" value="<?= $id ?>">
+                商品名<input type="text" name="name" value="<?= $name ?>"><br>
+
+                <!-- 単価と円を同じ行に配置 -->
+                単価
+                <div class="price-container">
+                    <input type="number" name="price" value="<?= $price ?>">
+                    <span class="currency">円</span>
+                </div><br>
+
+                <!-- 在庫と個を同じ行に配置 -->
+                在庫
+                <div class="stock-container">
+                    <input type="number" name="stock" value="<?= $stock ?>">
+                    <span class="unit">個</span>
+                </div><br>
+
+                オプション<input type="text" name="option" value="<?= $option ?>"><br>
+                カテゴリー<input type="text" name="category" value="<?= $category ?>"><br>
+                産地<input type="text" name="made" value="<?= $made ?>"><br>
+                販売元<input type="text" name="seller" value="<?= $seller ?>"><br>
+            </div>
+
+            <div class="form-right">
+                商品画像<br>
+                <?php
                 $imagePath = '/teamDev/uploads/' . $shohin_pict;
-                echo '<img src="' . $imagePath . '" alt="' . $name . '" width="220px" height="auto">';
+                echo '<img src="' . $imagePath . '" alt="' . $name . '" class="shohin__img">';
                 ?>
-            <input type="file" name="pict" style="width: 230px; height: 48px;"><br><br>
-            商品説明<input type="text" name="setumei" value="<?= $explain ?> style=" width: 500px; height: 114px;">
+                <input type="file" name="pict"><br>
 
-            産地<input type="text" name="santi" value="<?= $made ?>" style="width: 230px; height: 48px;"><br><br>
-            販売元<input type="text" name="hanbai" value="<?= $seller ?>" style="width: 230px; height: 48px;"><br>
+                商品説明<br>
+                <textarea name="explain"><?= $explain ?></textarea><br>
+            </div>
+        </div>
 
-            <input type="submit" value="戻る" >
-            <input type="submit" value="削除" >
-            <input type="submit" value="登録" >
+
+        <div class="button-container">
+            <a href="admin_top.php"><input type="submit" value="戻る"></a>
+            <input type="submit" name="dele" value="削除">
+            <input type="submit" name="up" value="登録">
+        </div>
+        <br>
+        <hr class="hr">
     </form>
 </body>
--- FurusatoMarche/admin_shohins_insert.php
+++ FurusatoMarche/admin_shohins_insert.php
@@ -1,5 +1,4 @@
 <?php
-ob_start();
 require_once 'function.php';
 $pdo = pdo();
 if (isset($_POST['in'])) {
@@ -56,7 +55,7 @@ if (isset($_POST['shohin_id'])) {
 
 // 削除処理
 if (isset($_POST['dele'])) {
-    $shohin_id = $_POST['shohin_id'];
+    $shohin_id = $_POST['id'];
     $sql = $pdo->prepare('DELETE FROM shohins WHERE shohin_id = ?');
     $sql->execute([$shohin_id]);
     header('Location: admin_top.php');
@@ -64,23 +63,24 @@ if (isset($_POST['dele'])) {
 }
 
 if (isset($_POST['up'])) {
-    $id = $_POST['shohin_id'];
-    $name = $_POST['shohin_name'];
-    $price = $_POST['shohin_price'];
-    $stock = $_POST['shohin_stock'];
-    $option = $_POST['shohin_option'];
-    $category = $_POST['shohin_category'];
-    $explain = $_POST['shohin_explain'];
-    $made = $_POST['shohin_made'];
-    $seller = $_POST['shohin_seller'];
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $stock = $_POST['stock'];
+    $option = $_POST['option'];
+    $category = $_POST['category'];
+    $explain = $_POST['explain'];
+    $made = $_POST['made'];
+    $seller = $_POST['seller'];
 
     $sql = $pdo->prepare('UPDATE shohins SET
                             shohin_name = ?, shohin_price = ?, shohin_stock = ?, shohin_option = ?,
                             shohin_category = ?,shohin_explain = ?, shohin_made = ?, shohin_seller = ?
                           WHERE shohin_id = ?');
-    $sql->execute([$name, $price, $stock, $option, $category, $explain, $made, $seller, $id]);
+    if (!($sql->execute([$name, $price, $stock, $option, $category, $explain, $made, $seller, $id]))) {
+    }
     header('Location: admin_top.php');
     exit;
 }
-ob_flush();
-echo 'なんも情報ないよ～';
+header('Location: admin_top.php');
+exit;
